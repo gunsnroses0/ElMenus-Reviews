1936602800db162c6e44e3cf9cc651abac73275b
Ali Waleed <ali.sobh@student.guc.edu.eg>
2019-04-23 23:31:47 +0200

    Update Review.java

--- src/Model/Review.java
+++ src/Model/Review.java
@@ -27,8 +27,8 @@ import Commands.Command;
 public class Review {
 
 	private static final String COLLECTION_NAME = "reviews";
-
-	static MongoClientURI uri = new MongoClientURI("mongodb://mongo-0.mongo,mongo-1.mongo,mongo-2.mongo:27017/El-Menus?");
+	String host = System.getenv("MONGO_URI");
+	static MongoClientURI uri = new MongoClientURI(host);
 
 	static MongoClient mongoClient = new MongoClient(uri);
 	static MongoDatabase database = mongoClient.getDatabase("El-Menus");
