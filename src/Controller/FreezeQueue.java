247624c0432787ebd241823b1224a6aa500485e0
ahmedash11 <ahmedashh11@gmail.com>
2019-04-26 17:04:13 +0200

    [Feature Implementation] Adding Controller Commands

--- src/Controller/FreezeQueue.java
+++ src/Controller/FreezeQueue.java
@@ -30,10 +30,10 @@ public class FreezeQueue extends Command {
 		HashMap<String, Object> props = parameters;
 		Channel channel = (Channel) props.get("channel");
 		try {
-			ReviewService.setRPC_QUEUE_NAME("like-request-DELETED");;
+			ReviewService.setRPC_QUEUE_NAME("review-request-DELETED");;
 			connection = factory.newConnection();
 			channel = connection.createChannel();
-			channel.queueDelete("like-request",false,false);
+			channel.queueDelete("review-request",false,false);
 			AMQP.BasicProperties properties = (AMQP.BasicProperties) props.get("properties");
 			AMQP.BasicProperties replyProps = (AMQP.BasicProperties) props.get("replyProps");
 			Envelope envelope = (Envelope) props.get("envelope");
