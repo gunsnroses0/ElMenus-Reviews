bade992010ce52e3957e4aa19fe2578f90bc28cf
ahmedash11 <ahmedashh11@gmail.com>
2019-04-26 14:56:43 +0200

    [Added Controller]

--- src/Service/ReviewService.java
+++ src/Service/ReviewService.java
@@ -1,9 +1,15 @@
+package Service;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeoutException;
+import java.util.regex.Pattern;
 
+import org.apache.commons.lang.StringUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -26,10 +32,19 @@ import Commands.Command;
 import Commands.*;
 
 public class ReviewService {
-	private static final String RPC_QUEUE_NAME = "review-request";
+	private static String RPC_QUEUE_NAME = "review-request";
 	public static  MongoDatabase database;
+	public static HashMap<String, String> config;
 	public static void main(String[] argv) {
-
+		run();
+	}
+	public static void run() {
+		try {
+			updateHashMap();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		// initialize thread pool of fixed size
 		final ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(4);
 
@@ -62,18 +77,26 @@ public class ReviewService {
 						String message = new String(body, "UTF-8");
 						JSONParser parser = new JSONParser();
 						JSONObject messageBody = (JSONObject) parser.parse(message);
-						String command = (String) messageBody.get("command");
-						Command cmd = null;
-						System.out.println(command);
-						switch (command) {
-						case "CreateReview":
-							cmd = new PostReview();
-							break;
+//						String service = StringUtils.substringsBetween((String) messageBody.get("uri"), "/", "/");
+						String [] URI = ((String) messageBody.get("uri")).split(Pattern.quote("/"));
+						String service= "";
+						for (int i =0; i<URI.length;i++) {
+							if (!(StringUtils.containsAny(URI[i],  new char []{'0','1','2','3','4','5','6','7','8','9'}))) {
+								service += URI[i]+"/";
+							}else {
+								service += "id";
 
-						case "RetrieveReview":
-							cmd = new GetReview();
+							}
 						}
-
+//						System.out.println((String) messageBody.get("uri"));
+//						StringUtils.containsAny(str, searchChars)
+						System.out.println("URI"+ URI[0]);
+						String key = (String) messageBody.get("request_method")+ service;
+						System.out.println("KEY"+ key );
+						System.out.println("config"+config.get(key));
+						String command = (String)config.get(key);
+						Command cmd = (Command) Class.forName("Commands."+command).newInstance();
+						System.out.println(cmd);
 						HashMap<String, Object> props = new HashMap<String, Object>();
 						props.put("channel", channel);
 						props.put("properties", properties);
@@ -86,6 +109,15 @@ public class ReviewService {
 					} catch (RuntimeException e) {
 						System.out.println(" [.] " + e.toString());
 					} catch (ParseException e) {
+						e.printStackTrace();
+					} catch (InstantiationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IllegalAccessException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (ClassNotFoundException e) {
+						// TODO Auto-generated catch block
 						e.printStackTrace();
 					} finally {
 						synchronized (this) {
@@ -101,7 +133,36 @@ public class ReviewService {
 		}
 
 	}
+	public static void updateHashMap() throws IOException {
+		config = new HashMap<String, String>();
+		System.out.println("X");
+		File file = new File("src/config");
+		BufferedReader br = new BufferedReader(new FileReader(file)); 
+		  
+		  String st; 
 
+		  while ((st = br.readLine()) != null) {
+			    System.out.println(st); 
+			  String[] array = st.split(",");
+			  config.put(array[0]+array[1],array[2]);
+		  }
+		  System.out.println(config);
+		 br.close(); 
+
+	}
+
+	public static MongoDatabase getDatabase() {
+		return database;
+	}
+	public static void setDatabase(MongoDatabase database) {
+		ReviewService.database = database;
+	}
+	public static String getRPC_QUEUE_NAME() {
+		return RPC_QUEUE_NAME;
+	}
+	public static void setRPC_QUEUE_NAME(String rPC_QUEUE_NAME) {
+		RPC_QUEUE_NAME = rPC_QUEUE_NAME;
+	}
 	public static String getCommand(String message) throws ParseException {
 		JSONParser parser = new JSONParser();
 		JSONObject messageJson = (JSONObject) parser.parse(message);
